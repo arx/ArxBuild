f1420311f22a3a25acebb3b09d9b78c160c33111
Daniel Scharrer <daniel@constexpr.org>
2013-02-14 16:25:55 +0100

    innoextract: Add armv6 build
    Also, use libc++ for 32-bit clang builds.

---
+++ toolchains/gcc-armv6j-hf-toolchain.cmake
@@ -0,0 +1,14 @@
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER "armv6j-hardfloat-linux-gnueabi-gcc")
+set(CMAKE_CXX_COMPILER "armv6j-hardfloat-linux-gnueabi-g++")
+
+# here is the target environment located
+set(CMAKE_FIND_ROOT_PATH "/usr/armv6j-hardfloat-linux-gnueabi")
+
+# adjust the default behaviour of the find_xxx() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
