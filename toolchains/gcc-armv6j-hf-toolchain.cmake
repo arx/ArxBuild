deabe8609e883c1b8044b4a43a51ee1d587cb187
Daniel Scharrer <daniel@constexpr.org>
2013-04-23 23:00:39 +0200

    Set the FPU ABI in arm builds

--- toolchains/gcc-armv6j-hf-toolchain.cmake
+++ toolchains/gcc-armv6j-hf-toolchain.cmake
@@ -1,4 +1,7 @@
 
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+
 # which compilers to use for C and C++
 set(CMAKE_C_COMPILER "armv6j-hardfloat-linux-gnueabi-gcc")
 set(CMAKE_CXX_COMPILER "armv6j-hardfloat-linux-gnueabi-g++")
@@ -12,3 +15,5 @@ set(CMAKE_FIND_ROOT_PATH "/usr/armv6j-hardfloat-linux-gnueabi")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+add_definitions(-mfpu=vfp -mfloat-abi=hard)
