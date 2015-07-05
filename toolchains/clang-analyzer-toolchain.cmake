b4fff76e9bbb9ca97a297c6c41500b5545704391
Daniel Scharrer <daniel@constexpr.org>
2015-07-05 21:19:28 +0200

    Generate detailed results for clang-analyzer

--- toolchains/clang-analyzer-toolchain.cmake
+++ toolchains/clang-analyzer-toolchain.cmake
@@ -1,4 +1,4 @@
 
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER "${CMAKE_CURRENT_LIST_DIR}/ccc-analyzer")
-set(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_LIST_DIR}/c++-analyzer")
+set(CMAKE_C_COMPILER "ccc-analyzer")
+set(CMAKE_CXX_COMPILER "c++-analyzer")
