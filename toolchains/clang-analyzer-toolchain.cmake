d713572bd42c5a7deebbea738333be5a29acf5e6
Daniel Scharrer <daniel@constexpr.org>
2015-12-28 21:16:45 +0100

    Fix clang-analyzer builds with newer LLVM installs

--- toolchains/clang-analyzer-toolchain.cmake
+++ toolchains/clang-analyzer-toolchain.cmake
@@ -1,4 +1,6 @@
 
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER "ccc-analyzer")
-set(CMAKE_CXX_COMPILER "c++-analyzer")
+
+find_program(CMAKE_C_COMPILER "ccc-analyzer" PATHS ${CMAKE_PREFIX_PATH}/libexec /usr/libexec)
+
+find_program(CMAKE_CXX_COMPILER "c++-analyzer" PATHS ${CMAKE_PREFIX_PATH}/libexec /usr/libexec)
